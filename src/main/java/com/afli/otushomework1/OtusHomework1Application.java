144199950faadc161fc2abb39b0302ffcac4d228
afli <il.gusevskiy@gmail.com>
2023-08-08 19:13:29 +0300

    initial commit

---
+++ src/main/java/com/afli/otushomework1/OtusHomework1Application.java
@@ -0,0 +1,13 @@
+package com.afli.otushomework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusHomework1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusHomework1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/afli/otushomework1/OtusHomework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afli.otushomework1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusHomework1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
